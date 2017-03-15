5d7ed79c2babff922abe18ca54bcf0182d552639
VadimVzorov <vzblazer@gmail.com>
2017-03-15 19:16:09 +0100

    log

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 print(os.environ)
-if 'HEROKU' in os.environ:
+if 'DATABASE_URL' in os.environ:
     # production on Heroku
-    config_path = os.environ.get("CONFIG_PATH", "blog.config.ProductionConfig")
+    config_path = os.environ["DATABASE_URL"]
 else:
     # development on localhost
     config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
--- blog/config.py
+++ blog/config.py
@@ -4,5 +4,4 @@ class DevelopmentConfig(object):
     DEBUG = True
 
 class ProductionConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgres://hzyqgyzzrjtaom:5dc8aa5c47529024ef8c40951ab1866dae63d120e9d512055ac96848f6473cfd@ec2-54-225-67-3.compute-1.amazonaws.com:5432/dates3p8m82mhl"
     DEBUG = True
