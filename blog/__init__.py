a1668ae0704e817c902d17de6fa48cc8e59677b9
VadimVzorov <vzblazer@gmail.com>
2017-03-15 18:59:16 +0100

    commit 3

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,7 +2,14 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+
+if 'HEROKU' in os.environ:
+    # production on Heroku
+    config_path = os.environ.get("CONFIG_PATH", "blog.config.ProductionConfig")
+else:
+    # development on localhost
+    config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+
 app.config.from_object(config_path)
 
 from . import views
--- blog/config.py
+++ blog/config.py
@@ -2,3 +2,7 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://vadimvzorov:@localhost:5432/blogful"
     DEBUG = True
+
+class ProductionConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgres://hzyqgyzzrjtaom:5dc8aa5c47529024ef8c40951ab1866dae63d120e9d512055ac96848f6473cfd@ec2-54-225-67-3.compute-1.amazonaws.com:5432/dates3p8m82mhl"
+    DEBUG = True
