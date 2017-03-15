d20fb0475340963b1ed52bf2f79a726b524c9741
VadimVzorov <vzblazer@gmail.com>
2017-03-15 19:02:41 +0100

    log

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-
+print(os.environ)
 if 'HEROKU' in os.environ:
     # production on Heroku
     config_path = os.environ.get("CONFIG_PATH", "blog.config.ProductionConfig")
